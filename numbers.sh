899e3af1fd9513137165ca02a6355b9ddf1fe9a1
Joshua Barrs <joshuab@silverlion.net>
2019-04-26 06:56:44 +0000

    I'm comitting

--- numbers.sh
+++ numbers.sh
@@ -1,20 +1,23 @@
-#!/bin/bash 
+#! /bin/bash 
+#numbers.sh
+#Joshua Barrs
 
 echo "Please enter a number: " 
 
 read number
 
-n  = 0
+n=0
 
-while n<=number 
+while [ $n -le $number ] 
 do 
-	if $((n%2)) -eq 0
-		echo "Even"
+	if [ $((n%2)) -eq 0 ]
+	then
+		echo $n "Even"
 	else
-		echo "Odd"
+		echo $n "Odd"
 	fi 
 	
-	n = $[$n+1]
+	n=$[$n+1]
 done 
 
 	 
